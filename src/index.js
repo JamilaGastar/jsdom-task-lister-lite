22c8464c9f071c655b95d3d0fead8ec7151f3d43
jamilagastar <jamilagastar@gmail.com>
2022-05-05 15:46:29 +1000

    created functional checklist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.getElementById('create-task-form')
+  const tasks = document.getElementById('tasks');
+  const input = document.getElementById('new-task-description');
+  const submitButton = document.getElementById('submit-button');
+
+  submitButton.addEventListener("click", function(event){
+    event.preventDefault();
+    let commentInputValue = input.value;
+
+    let newTask = document.createElement('li');
+    let text = document.createTextNode(commentInputValue)
+    newTask.appendChild(text);
+    tasks.appendChild(newTask);
+    form.reset();
+})
+
+})
+
+
+
